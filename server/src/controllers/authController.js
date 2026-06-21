2149b1222d3cf2f44bb044ae87b29f303300d066
EdwinFestus <edysupexz@gmail.com>
2026-06-21 13:48:18 +0200

    BACKEND - Login, Hashing

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -35,4 +35,40 @@ export const registerUser = async (req, res) => {
     }
 }
 
-export default registerUser
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid Credentials!!!",
+            })
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+       
+        if( !isMatch ) {
+            return res.status(401).json({
+                message: "Invalid Credentials !!!",
+            })
+        }
+      
+        res.status(200).json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } catch(err) {
+        res.status(500).json({
+            message: err.message,
+        })
+    }
+}
+
